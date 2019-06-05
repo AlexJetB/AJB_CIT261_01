a9b459de65afac85388300f05a3907ea92c941ef
Alex Baker <baker.alex.j@gmail.com>
2019-06-05 16:22:23 -0600

    File dump.

---
+++ EJS/Week07/Examples/ch11Example.js
@@ -0,0 +1,24 @@
+var bigOak = require("./crow-tech").bigOak;
+
+async function locateScalpel(nest) {
+  let current = nest.name;
+  for (;;) {
+    let next = await anyStorage(nest, current, "scalpel");
+    if (next == current) return current;
+    current = next;
+  }
+}
+
+function locateScalpel2(nest) {
+  function loop(current) {
+    return anyStorage(nest, current, "scalpel").then(next=> {
+      if (next == current) return current;
+      else return loop(next);
+    })
+  }
+  return loop(nest.name);
+}
+
+locateScalpel(bigOak).then(console.log);
+// → Butcher Shop
+locateScalpel(bigOak).then(console.log);
